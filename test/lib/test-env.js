489bc9bb75fcd102e8a9c50574749d185ab9026b
meeksfred <maxfred0025@gmail.com>
2017-03-09 15:32:03 -0800

    added error, basic auth, and bearer auth middleware. started a test env file

---
+++ test/lib/test-env.js
@@ -0,0 +1,8 @@
+'use strict';
+
+process.env.TITLE = 'givegood test';
+process.env.MONGODB_URI = 'mongodb://localhost/givegoodtesting';
+process.env.PORT = 3000;
+process.env.NODE_ENV = 'testing';
+process.env.APP_SECRET = 'app secret for testing';
+process.env.API_URL = 'http://localhost:3000';
