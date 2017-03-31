a05617ba5a6ab585a7eb99fa378a2ab47f5d19dd
meeksfred <maxfred0025@gmail.com>
2017-03-31 13:59:44 -0700

    landing page route

--- app/config/router-config.js
+++ app/config/router-config.js
@@ -1,1 +1,22 @@
 'use strict';
+
+module.exports = ['$stateProvider', '$urlRouterProvider', routerConfig];
+
+function routerConfig($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.when('' , '/landing');
+  $urlRouterProvider.when('/' , '/landing');
+
+  let states = [
+    {
+      name: 'welcome',
+      url: '/landing',
+      controllerAs: 'landingCtrl',
+      controller: 'LandingController',
+      template: require('./view/landing/landing.html'),
+    },
+  ];
+
+  states.forEach(state => {
+    $stateProvider.state(state);
+  });
+}
