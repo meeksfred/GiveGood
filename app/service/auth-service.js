4c997d9a0e6be2480de4898d28f17e45832563d7
meeksfred <maxfred0025@gmail.com>
2017-04-04 13:27:21 -0700

    added getToken and signup functionality

--- app/service/auth-service.js
+++ app/service/auth-service.js
@@ -9,14 +9,58 @@ function authService($q, $log, $http, $window) {
   let token = null;
 
   service.setToken = function(_token) {
-    $log.debug('authService.service.setToken()');
+    $log.debug('authService.setToken()');
 
-    if (!_token)
-      return $q.reject(new Error('no service.token'));
-
+    if (!_token) return $q.reject(new Error('no token'));
     $window.localStorage.setItem('token', _token);
-    service.token = _token;
-    return $q.resolve(service.token);
+    token = _token;
+    return $q.resolve(token);
+  };
+
+  service.getToken = function() {
+    $log.debug('authService.getToken()');
+
+    if(token) return $q.resolve(token);
+    token = $window.localStorage.getItem('token');
+    if(token) return $q.resolve(token);
+    return $q.reject(new Error('token not found'));
+  };
+
+  service.logout = function() {
+    $log.debug('authService.logout()');
+
+    $window.localStorage.removeItem('token');
+    token = null;
+    return $q.resolve();
+  };
+
+  service.signup = function(user) {
+    $log.debug('authService.signup()');
+
+    let url = `${__API_URL__}/api/signup`;
+
+    let config = {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+    };
+
+    return $http.post(url, user, config)
+    .then( res => {
+      $log.debug('success', res.data);
+      return service.setToken(res.data);
+    })
+    .catch( err => {
+      $log.error(err.message);
+      return $q.reject(err);
+    });
+  };
+
+  service.login = function(user) {
+    $log.debug('service.login()');
+
+
   };
 
   return service;
