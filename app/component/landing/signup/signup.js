7751a7b72570fd5bf8fed30971efa068a0d61047
meeksfred <maxfred0025@gmail.com>
2017-04-04 13:47:40 -0700

    created signup component, restructured landing component directory

---
+++ app/component/landing/signup/signup.js
@@ -0,0 +1,24 @@
+'use strict';
+
+require('./_signup.scss');
+
+module.exports = {
+  template: require('./signup.html'),
+  controller: ['$log', '$location', 'authService', SignupController],
+  controllerAs: 'signupCtrl',
+};
+
+function SignupController($log, $location, authService) {
+  $log.debug('init signupCtrl');
+
+  this.signup = function(user) {
+    authService.signup(user)
+    .then( () => {
+      $location.path('/home');
+    })
+    .catch( () => {
+      $log.debug('signup failed');
+    });
+  };
+
+}
