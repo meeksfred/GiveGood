2abfd401ae77e36b81df8074b0ed6f3bc1cfe640
meeksfred <maxfred0025@gmail.com>
2017-04-20 10:29:12 -0700

    leaving messages for next TODO

--- app/view/home/home-controller.js
+++ app/view/home/home-controller.js
@@ -9,6 +9,8 @@ function HomeController($log, $location, authService) {
 
   this.data; // need to make a GET route for a profile to get data of user on Home view. Not sure I can make a binding from the login/signup components
 
+  // once I have user data within the home view, setup a simple dropdown or button for now to make sure deleting a user account works. send the view back to landing on successful deletion.
+
   this.logout = function() {
     $log.debug('homeCtrl.logout()');
     authService.logout()
